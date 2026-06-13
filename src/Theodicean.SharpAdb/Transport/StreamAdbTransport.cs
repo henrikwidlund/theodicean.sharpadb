57f8a79c5143cc3173f5c1d2bf04c869f775f0a0
Henrik Widlund <4659350+henrikwidlund@users.noreply.github.com>
2026-06-13 23:31:35 +0200

    Lint

--- src/Theodicean.SharpAdb/Transport/StreamAdbTransport.cs
+++ src/Theodicean.SharpAdb/Transport/StreamAdbTransport.cs
@@ -1,4 +1,5 @@
 using System.Buffers;
+using System.Globalization;
 using System.Net.Security;
 using System.Net.Sockets;
 using System.Security.Cryptography.X509Certificates;
@@ -78,7 +79,7 @@ public sealed class StreamAdbTransport : IAdbTransport
             // SslStream as bogus TLS records. Catch obvious cases of that and fail fast.
             if (_stream is NetworkStream { DataAvailable: true } ns)
                 throw new InvalidDataException(
-                    $"Refusing TLS upgrade: {ns.Socket.Available} bytes pending on transport (peer violated STLS framing)");
+                    string.Create(CultureInfo.InvariantCulture, $"Refusing TLS upgrade: {ns.Socket.Available} bytes pending on transport (peer violated STLS framing)"));
 
             var ssl = new SslStream(_stream, leaveInnerStreamOpen: false);
 
